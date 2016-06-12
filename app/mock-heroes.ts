d55e11d8db2caf253552aa48bf4af9502162ba12
Yogesh Manware <yogesh.manware@sungard.com>
2016-06-12 11:39:05 +0530

    first commit code

---
+++ app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export var HEROES1: Hero[] = [
+  { 'id': 11, 'name': 'Mr. Nice' },
+  { 'id': 12, 'name': 'Narco' },
+  { 'id': 13, 'name': 'Bombasto' },
+  { 'id': 14, 'name': 'Celeritas' },
+  { 'id': 15, 'name': 'Magneta' },
+  { 'id': 16, 'name': 'RubberMan' },
+  { 'id': 17, 'name': 'Dynama' },
+  { 'id': 18, 'name': 'Dr IQ' },
+  { 'id': 19, 'name': 'Magma' },
+  { 'id': 20, 'name': 'Tornado' }
+];
+
+export var HEROES: Hero[] = [
+  { 'id': 1, 'name': 'F.C. Road' },
+  { 'id': 2, 'name': 'M.G. Road' },
+  { 'id': 3, 'name': 'J.M Road' },
+  { 'id': 4, 'name': 'WESTEND - FIS' },
+  { 'id': 5, 'name': 'Station Road' },
+  { 'id': 6, 'name': 'Kothrud' },
+  { 'id': 7, 'name': 'Kalyani Nagar' }
+];
